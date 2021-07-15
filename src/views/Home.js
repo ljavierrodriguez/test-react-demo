d71a92036b5b7a7b107bf065c0c107781622c24b
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2021-07-15 01:46:09 +0000

    Modifcacion Vista Home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,12 @@
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Home</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
